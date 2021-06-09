07d9919ea0202345bfa14e5eff3d7935007829b1
ionitarobert97 <ionita.robertstefan97@gmail.com>
2021-06-09 16:31:20 +0300

    React, pages and navbar added

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Vacation App",
+  icon: "fas fa-map-marked-alt",
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this App</h1>
+            <p className="my-1st-p">
+                This is a full stack React app for vacation memories.
+            </p>
+            <p>
+                <strong>Version: </strong> 0.0.1
+            </p>
+        </div>
+    )
+}
+
+export default About;
